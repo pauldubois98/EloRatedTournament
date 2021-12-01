bcfb2bf706e987c1ba3f78b7b632cd1738a93a44
pauldubois98 <pauldubois98@gmail.com>
2021-12-01 23:54:26 +0100

    Elo calculation

---
+++ Elo.js
@@ -0,0 +1,34 @@
+
+
+function Elo_history(teams, games, K=20){
+    console.log(teams)
+    var history = []
+    var Elos = Array(...teams);
+    Elos.fill(400)
+    history = history.concat([Elos]);
+    console.log(Elos)
+    for(var i=0; i<games.length; i++){
+        if(games[i][0] != games[i][2]){
+            var teamA_Elo = Elos[games[i][0]];
+            var teamB_Elo = Elos[games[i][2]];
+            var scoreA = 0.5;
+            var scoreB = 0.5;
+            if(games[i][1]=="\>"){
+                scoreA = 1;
+                scoreB = 0;
+            }
+            if(games[i][1]=="\<"){
+                scoreA = 0;
+                scoreB = 1;
+            }
+            var expected_scoreA = 1 / (1 + 10**((teamB_Elo - teamA_Elo)/200))
+            var expected_scoreB = 1 / (1 + 10**((teamA_Elo - teamB_Elo)/200))
+            teamA_Elo = teamA_Elo + K * (scoreA-expected_scoreA)
+            teamB_Elo = teamB_Elo + K * (scoreB-expected_scoreB)
+            Elos[games[i][0]] = teamA_Elo;
+            Elos[games[i][2]] = teamB_Elo;
+            history = history.concat([Elos]);
+            console.log(Elos)
+        }
+    }
+}
